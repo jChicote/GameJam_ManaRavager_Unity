881b117da9c926775e13451d1fe38b3c88aeb23d
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-09-09 20:00:43 +1000

    Add player socket positions, add sword animations

---
+++ Assets/Scripts/Player/Animations/PlayerCharacterSockets.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PlayerCharacterSockets : MonoBehaviour
+{
+
+    public Transform RightHandSocket;
+    public Transform LeftHandSocket;
+    public Transform SwordHolderSocket;
+
+}
---
+++ Assets/Scripts/Player/Weapons/PlayerSwordHandler.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerSwordHandler : MonoBehaviour
+{
+
+    public Animator PlayerAnimator;
+    public PlayerCharacterSockets CharacterSockets;
+
+    public void AnimationEvent_EquipSword()
+    {
+
+    }
+
+    public void AnimationEvent_SheathSword()
+    {
+
+    }
+
+}
