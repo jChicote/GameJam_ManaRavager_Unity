ed8c490325a1135fb07d7e355f6fce2f740eb046
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-09-11 23:07:16 +1000

    Create animation controller for enemy

--- Assets/Scripts/Enemy/BehaviourTree/Tasks/Navigation/AIFollowTarget.cs
+++ Assets/Scripts/Enemy/BehaviourTree/Tasks/Navigation/AIFollowTarget.cs
@@ -8,13 +8,25 @@ public class AIFollowTarget : Leaf
 {
 
     public NavMeshAgent Agent;
-
+    public Animator Animator;
     public TransformReference FollowTarget = new TransformReference();
+
+    private float _animationMotionSpeedBlendTime;
 
     public override NodeResult Execute()
     {
-        Debug.Log("adawdw");
         Agent.SetDestination(FollowTarget.Value.position);
+
+        var speed = Agent.velocity.magnitude;
+        var blendTarget = speed > 2.5f ? 1f : 0.5f;
+
+        _animationMotionSpeedBlendTime = Mathf.Lerp(
+            _animationMotionSpeedBlendTime,
+            blendTarget,
+            Time.deltaTime * 10f);
+
+        Animator.SetFloat(EnemyAnimationParams.MotionSpeed, _animationMotionSpeedBlendTime);
+
         return NodeResult.success;
     }
 
