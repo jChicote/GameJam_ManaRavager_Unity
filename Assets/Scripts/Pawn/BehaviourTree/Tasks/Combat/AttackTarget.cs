013e526ce2a066d9587ea819b2e41851664a547d
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-09-29 23:57:30 +1000

    Implement AttackTarget behaviour

--- Assets/Scripts/Pawn/BehaviourTree/Tasks/Combat/AttackTarget.cs
+++ Assets/Scripts/Pawn/BehaviourTree/Tasks/Combat/AttackTarget.cs
@@ -8,16 +8,26 @@ public class AttackTarget : Leaf
 
     public TransformReference TargetTransform = new TransformReference();
     public TransformReference SelfTransform = new TransformReference();
+    public FloatReference MovementSpeed = new FloatReference();
 
     public PawnSwordHandler SwordHandler;
 
-    public float DesiredDistance = 2.5f;
-    public float ErrorTolerance = 0.5f;
+    public float DesiredDistance = 0.5f;
+    public float ErrorTolerance = 0.25f;
 
     public override NodeResult Execute()
     {
         var direction = transform.position - TargetTransform.Value.position;
         var distance = direction.magnitude;
+
+        if (distance > DesiredDistance + ErrorTolerance)
+        {
+            SelfTransform.Value.position = Vector3.MoveTowards(
+                SelfTransform.Value.position,
+                TargetTransform.Value.position,
+                MovementSpeed.Value * Time.deltaTime);
+            return NodeResult.running;
+        }
 
         if (Mathf.Abs(distance - DesiredDistance) <= ErrorTolerance)
         {
