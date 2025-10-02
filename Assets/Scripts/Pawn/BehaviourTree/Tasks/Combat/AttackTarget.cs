ff5b1269a6b7f8a6e9eb4adde55818909cc9e81d
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-10-03 08:28:09 +1000

    Update attack logic to include moving towards the player for close up attack

--- Assets/Scripts/Pawn/BehaviourTree/Tasks/Combat/AttackTarget.cs
+++ Assets/Scripts/Pawn/BehaviourTree/Tasks/Combat/AttackTarget.cs
@@ -1,11 +1,14 @@
 using MBT;
 using UnityEngine;
+using UnityEngine.AI;
 
 [AddComponentMenu("")]
 [MBTNode(name = "Tasks/Attack Target")]
 public class AttackTarget : Leaf
 {
 
+    public NavMeshAgent Agent;
+    public Animator Animator;
     public TransformReference TargetTransform = new TransformReference();
     public TransformReference SelfTransform = new TransformReference();
     public FloatReference MovementSpeed = new FloatReference();
@@ -14,6 +17,8 @@ public class AttackTarget : Leaf
 
     public float DesiredDistance = 0.5f;
     public float ErrorTolerance = 0.25f;
+
+    private float _animationMotionSpeedBlendTime;
 
     public override NodeResult Execute()
     {
@@ -26,6 +31,15 @@ public class AttackTarget : Leaf
                 SelfTransform.Value.position,
                 TargetTransform.Value.position,
                 MovementSpeed.Value * Time.deltaTime);
+
+            // Move to target
+            _animationMotionSpeedBlendTime = Mathf.Lerp(
+                _animationMotionSpeedBlendTime,
+                Agent.velocity.magnitude / MovementSpeed.Value,
+                Time.deltaTime * 10f);
+
+            Animator.SetFloat(EnemyAnimationParams.MotionSpeed, _animationMotionSpeedBlendTime);
+
             return NodeResult.running;
         }
 
