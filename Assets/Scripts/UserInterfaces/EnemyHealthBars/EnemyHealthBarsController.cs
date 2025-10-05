d0197ad1d8f4a536d05a1d2491445425640ddbb8
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-10-05 23:10:37 +1100

    Add EnemyHealthBars logic

--- Assets/Scripts/UserInterfaces/EnemyHealthBars/EnemyHealthBarsController.cs
+++ Assets/Scripts/UserInterfaces/EnemyHealthBars/EnemyHealthBarsController.cs
@@ -1,6 +1,52 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHealthBarsController : MonoBehaviour
 {
 
+    public UIEventHub EventHub;
+    public EnemyHealthBarsView View;
+
+    private readonly Dictionary<int, ICharacterHealth> _activeEnemyHealthBars = new();
+
+    private void Start()
+    {
+        EventHub.RegisterEvent(
+            "AddEnemyHealthBar",
+            request => this.AddEnemyHealthBar(request as CreateEnemyHealthBarRequest));
+
+        EventHub.RegisterEvent(
+            "UpdateEnemyHealthBar",
+            identifier => this.UpdateEnemyHealthBar((int)identifier));
+
+        EventHub.RegisterEvent(
+            "RemoveEnemyHealthBar",
+            identifier => this.RemoveEnemyHealthBar((int)identifier));
+    }
+
+    public void AddEnemyHealthBar(CreateEnemyHealthBarRequest request)
+    {
+        _activeEnemyHealthBars.Add(request.Identifier, request.EnemyHealth);
+        View.AddEnemyHealthBar(request.Identifier, request.EnemyHealth.CurrentHealth, request.EnemyHealth.MaxHealth);
+    }
+
+    public void UpdateEnemyHealthBar(int identifier)
+    {
+        var _characterHealth = _activeEnemyHealthBars[identifier];
+        View.UpdateEnemyHealthBar(identifier, _characterHealth.CurrentHealth, _characterHealth.MaxHealth);
+    }
+
+    public void RemoveEnemyHealthBar(int identifier)
+    {
+        if (!_activeEnemyHealthBars.ContainsKey(identifier)) return;
+        _activeEnemyHealthBars.Remove(identifier);
+        View.RemoveEnemyHealthBar(identifier);
+    }
+
+}
+
+public class CreateEnemyHealthBarRequest
+{
+    public int Identifier;
+    public ICharacterHealth EnemyHealth;
 }
--- Assets/Scripts/UserInterfaces/EnemyHealthBars/EnemyHealthBarsView.cs
+++ Assets/Scripts/UserInterfaces/EnemyHealthBars/EnemyHealthBarsView.cs
@@ -1,8 +1,39 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHealthBarsView : MonoBehaviour
 {
 
-    private Dictionary<>
+    public GameObject HealthBarPrefab;
+
+    [Header("References")]
+    public GameObject ContentGroup;
+
+    private readonly Dictionary<int, HealthBar> _enemyHealthBars = new();
+
+    public void AddEnemyHealthBar(int identifier, float currentHealth, float maxHealth)
+    {
+        if (_enemyHealthBars.ContainsKey(identifier)) return;
+
+        var healthBar = Instantiate(HealthBarPrefab, ContentGroup.transform)
+            .GetComponent<HealthBar>();
+        _enemyHealthBars.Add(identifier, healthBar);
+
+        healthBar.SetHealth(currentHealth, maxHealth);
+    }
+
+    public void UpdateEnemyHealthBar(int identifier, float currentHealth, float maxHealth)
+    {
+        if (!_enemyHealthBars.TryGetValue(identifier, out var healthBar)) return;
+        healthBar.SetHealth(currentHealth, maxHealth);
+    }
+
+    public void RemoveEnemyHealthBar(int identifier)
+    {
+        if (!_enemyHealthBars.TryGetValue(identifier, out var healthBar)) return;
+
+        Destroy(healthBar.gameObject);
+        _enemyHealthBars.Remove(identifier);
+    }
 
 }
